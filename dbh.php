4723ba87d60a7163b17351217b625c9f4b6ffa77
Jack-in <64306586+Jack-in@users.noreply.github.com>
2022-02-02 11:00:32 +0530

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -11,7 +11,9 @@ define('DB_HOST', 'localhost');
 // $dBPassword = "";
 // $dbName = "register_db";
 
-$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+$conn = mysqli_init(); mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL); mysqli_real_connect($conn, "registeration-server.mysql.database.azure.com", "ghddgethsm", "{your_password}", "{your_database}", 3306, MYSQLI_CLIENT_SSL);
+
+// $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 mysqli_set_charset($conn, 'utf8');
 if ($conn->connect_error) {
